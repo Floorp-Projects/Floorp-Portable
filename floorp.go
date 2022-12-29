b7a4a19247b74a453c7fb25b0599a91d9b80b95a
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-12-30 04:37:19 +0900

    Fix indent

--- floorp.go
+++ floorp.go
@@ -1,48 +1,48 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "os/exec"
-    "path/filepath"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
-    args := os.Args[1:]
+	args := os.Args[1:]
 
-    exe, err := os.Executable()
-    if err != nil {
-        panic(err)
-    }
-    exe_dir := filepath.Dir(exe)
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exe_dir := filepath.Dir(exe)
 
-    if _, err := os.Stat(exe_dir + "/update_tmp/CORE_UPDATE_READY"); err == nil {
-        fmt.Println("Updates found.")
-        if err := os.Rename(exe_dir + "/core", exe_dir + "/core_old"); err == nil {
-            err := os.Remove(exe_dir + "/update_tmp/CORE_UPDATE_READY")
-            if err != nil {
-                panic(err)
-            }
-            err = os.Rename(exe_dir + "/update_tmp/core", exe_dir + "/core")
-            if err != nil {
-                panic(err)
-            }
-            err = os.RemoveAll(exe_dir + "/core_old")
-            if err != nil {
-                panic(err)
-            }
-            file, err := os.Create(exe_dir + "/update_tmp/REDIRECTOR_UPDATE_READY")
-            if err != nil {
-                panic(err)
-            }
-            file.Close()
-        } else {
-            fmt.Println("Floorp is running.")
-        }
-    }
+	if _, err := os.Stat(exe_dir + "/update_tmp/CORE_UPDATE_READY"); err == nil {
+		fmt.Println("Updates found.")
+		if err := os.Rename(exe_dir + "/core", exe_dir + "/core_old"); err == nil {
+			err := os.Remove(exe_dir + "/update_tmp/CORE_UPDATE_READY")
+			if err != nil {
+				panic(err)
+			}
+			err = os.Rename(exe_dir + "/update_tmp/core", exe_dir + "/core")
+			if err != nil {
+				panic(err)
+			}
+			err = os.RemoveAll(exe_dir + "/core_old")
+			if err != nil {
+				panic(err)
+			}
+			file, err := os.Create(exe_dir + "/update_tmp/REDIRECTOR_UPDATE_READY")
+			if err != nil {
+				panic(err)
+			}
+			file.Close()
+		} else {
+			fmt.Println("Floorp is running.")
+		}
+	}
 
-    err = exec.Command(exe_dir + "/core/floorp", args...).Run()
-    if err != nil {
-        panic(err)
-    }
+	err = exec.Command(exe_dir + "/core/floorp", args...).Run()
+	if err != nil {
+		panic(err)
+	}
 }
--- patcher.go
+++ patcher.go
@@ -1,90 +1,90 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
-    "os/exec"
-    "path/filepath"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path/filepath"
 )
 
 func main() {
-    exe, err := os.Executable()
-    if err != nil {
-        panic(err)
-    }
-    exe_dir := filepath.Dir(exe)
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exe_dir := filepath.Dir(exe)
 
 
-    out, err := exec.Command(exe_dir + "/utils/setdll64.exe", "/d:portable64.dll", exe_dir + "/core/mozglue.dll").Output()
-    fmt.Println(string(out))
-    if err != nil {
-        panic(err)
-    }
+	out, err := exec.Command(exe_dir + "/utils/setdll64.exe", "/d:portable64.dll", exe_dir + "/core/mozglue.dll").Output()
+	fmt.Println(string(out))
+	if err != nil {
+		panic(err)
+	}
 
 
-    src, err := os.Open(exe_dir + "/utils/portable64.dll")
-    if err != nil {
-        panic(err)
-    }
-    defer src.Close()
+	src, err := os.Open(exe_dir + "/utils/portable64.dll")
+	if err != nil {
+		panic(err)
+	}
+	defer src.Close()
 
-    dst, err := os.Create(exe_dir + "/core/portable64.dll")
-    if err != nil {
-        panic(err)
-    }
-    defer dst.Close()
+	dst, err := os.Create(exe_dir + "/core/portable64.dll")
+	if err != nil {
+		panic(err)
+	}
+	defer dst.Close()
 
-    _, err = io.Copy(dst, src)
-    if  err != nil {
-        panic(err)
-    }
-
-
-    src, err = os.Open(exe_dir + "/config/portable.ini")
-    if err != nil {
-        panic(err)
-    }
-    defer src.Close()
-
-    dst, err = os.Create(exe_dir + "/core/portable.ini")
-    if err != nil {
-        panic(err)
-    }
-    defer dst.Close()
-
-    _, err = io.Copy(dst, src)
-    if  err != nil {
-        panic(err)
-    }
+	_, err = io.Copy(dst, src)
+	if  err != nil {
+		panic(err)
+	}
 
 
-    if _, err := os.Stat(exe_dir + "/core/distribution"); err != nil {
-        err := os.Mkdir(exe_dir + "/core/distribution", 0777)
-        if err != nil {
-            panic(err)
-        }
-    }
+	src, err = os.Open(exe_dir + "/config/portable.ini")
+	if err != nil {
+		panic(err)
+	}
+	defer src.Close()
 
-    src, err = os.Open(exe_dir + "/config/policies.json")
-    if err != nil {
-        panic(err)
-    }
-    defer src.Close()
+	dst, err = os.Create(exe_dir + "/core/portable.ini")
+	if err != nil {
+		panic(err)
+	}
+	defer dst.Close()
 
-    dst, err = os.Create(exe_dir + "/core/distribution/policies.json")
-    if err != nil {
-        panic(err)
-    }
-    defer dst.Close()
-
-    _, err = io.Copy(dst, src)
-    if  err != nil {
-        panic(err)
-    }
+	_, err = io.Copy(dst, src)
+	if  err != nil {
+		panic(err)
+	}
 
 
-    os.Remove(exe_dir + "/core/updater.exe")
-    os.Remove(exe_dir + "/core/default-browser-agent.exe")
-    os.RemoveAll(exe_dir + "/core/uninstall")
+	if _, err := os.Stat(exe_dir + "/core/distribution"); err != nil {
+		err := os.Mkdir(exe_dir + "/core/distribution", 0777)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	src, err = os.Open(exe_dir + "/config/policies.json")
+	if err != nil {
+		panic(err)
+	}
+	defer src.Close()
+
+	dst, err = os.Create(exe_dir + "/core/distribution/policies.json")
+	if err != nil {
+		panic(err)
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if  err != nil {
+		panic(err)
+	}
+
+
+	os.Remove(exe_dir + "/core/updater.exe")
+	os.Remove(exe_dir + "/core/default-browser-agent.exe")
+	os.RemoveAll(exe_dir + "/core/uninstall")
 }
