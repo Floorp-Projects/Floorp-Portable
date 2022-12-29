4af5cb8ab53c6974c374a004a416ffef71e0884e
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2022-12-30 01:38:23 +0900

    Support for core update.

--- floorp.go
+++ floorp.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "fmt"
     "os"
     "os/exec"
     "path/filepath"
@@ -14,6 +15,26 @@ func main() {
         panic(err)
     }
     exe_dir := filepath.Dir(exe)
+
+    if _, err := os.Stat(exe_dir + "/update_tmp/UPDATE_READY"); err == nil {
+        fmt.Println("Updates found.")
+        if err := os.Rename(exe_dir + "/core", exe_dir + "/core_old"); err == nil {
+            err := os.Rename(exe_dir + "/update_tmp/core", exe_dir + "/core")
+            if err != nil {
+                panic(err)
+            }
+            err = os.Remove(exe_dir + "/update_tmp/UPDATE_READY")
+            if err != nil {
+                panic(err)
+            }
+            err = os.RemoveAll(exe_dir + "/update_tmp")
+            if err != nil {
+                panic(err)
+            }
+        } else {
+            fmt.Println("Floorp is running.")
+        }
+    }
 
     err = exec.Command(exe_dir + "/core/floorp", args...).Run()
     if err != nil {
