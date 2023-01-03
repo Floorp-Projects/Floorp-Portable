bf86f27454a1818e4e1a8358d890c7bf666bf0b8
typeling1578 <59644555+typeling1578@users.noreply.github.com>
2023-01-03 16:10:46 +0900

    Reduce binary size.

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Building redirector...
-go build -ldflags="-H windowsgui"
+go build -ldflags="-H windowsgui -s -w"
 echo Building patcher...
 cd patcher
-go build
+go build -ldflags="-s -w"
 copy patcher.exe ..
 cd ..
