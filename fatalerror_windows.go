9965d483f6ad5704746beb996772741cf6510bf8
typeling1578 <typeling1578@gmail.com>
2023-01-26 22:42:21 +0900

    Prepare to support for Linux version.

---
+++ fatalerror_windows.go
@@ -0,0 +1,22 @@
+// +build windows
+
+package main
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+func showFatalError(title string, message string) {
+	const (
+		NULL = 0x00000000
+		MB_OK = 0x00000000
+		MB_ICONERROR = 0x00000010
+	)
+	syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(
+		NULL,
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(message))),
+		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
+		MB_OK + MB_ICONERROR,
+	)
+}
--- main.go
+++ main.go
@@ -5,23 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"syscall"
-	"unsafe"
 )
-
-func showFatalError(title string, message string) {
-	const (
-		NULL = 0x00000000
-		MB_OK = 0x00000000
-		MB_ICONERROR = 0x00000010
-	)
-	syscall.NewLazyDLL("user32.dll").NewProc("MessageBoxW").Call(
-		NULL,
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(message))),
-		uintptr(unsafe.Pointer(syscall.StringToUTF16Ptr(title))),
-		MB_OK + MB_ICONERROR,
-	)
-}
 
 func main() {
 	args := os.Args[1:]
