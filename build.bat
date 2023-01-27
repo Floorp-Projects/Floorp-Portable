a649765a2910db55893f7f4cffe7e57fec9add6d
typeling1578 <typeling1578@gmail.com>
2023-01-27 21:44:15 +0900

    If the build fails, return exit code 1.

--- build.bat
+++ build.bat
@@ -1,8 +1,19 @@
 @echo off
+
 echo Building redirector...
 go build -ldflags="-H windowsgui -s -w"
+if %errorlevel% neq 0 (
+    exit /b 1
+)
+
 echo Building patcher...
 cd patcher
 go build -ldflags="-s -w"
+if %errorlevel% neq 0 (
+    exit /b 1
+)
 copy patcher.exe ..
+if %errorlevel% neq 0 (
+    exit /b 1
+)
 cd ..
