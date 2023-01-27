082eda193472539f743a5445ec50603c2edd53c9
typeling1578 <typeling1578@gmail.com>
2023-01-27 20:27:47 +0900

    Simplify the code.

--- fatalerror_linux.go
+++ fatalerror_linux.go
@@ -9,17 +9,8 @@ import (
 
 func showFatalError(title string, message string) {
 	// zenity or its fallback
-	zenity_command := ""
 	for _, command := range []string{"zenity", "matedialog", "qarma"} {
-		if _, err := exec.LookPath(command); err != nil {
-			log.Println("[ERROR]", err)
-		} else {
-			zenity_command = command
-			break
-		}
-	}
-	if zenity_command != "" {
-		err := exec.Command(zenity_command, "--error", "--title", title, "--text", message).Run()
+		err := exec.Command(command, "--error", "--title", title, "--text", message).Run()
 		if err == nil {
 			return
 		}
