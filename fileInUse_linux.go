ba8ae1e5c6f131879216a5f7f0199e45aba7c354
typeling1578 <typeling1578@gmail.com>
2023-03-11 00:36:48 +0900

    Fixed a problem that prevented Floorp from starting in the Linux version when a warning was output by the lsof command.

--- fileInUse_linux.go
+++ fileInUse_linux.go
@@ -14,7 +14,7 @@ func fileInUse(path string) bool {
 		log.Println("[ERROR]", err)
 		return false
 	}
-	out, err := exec.Command("lsof", path).CombinedOutput()
+	out, err := exec.Command("lsof", "-w", path).CombinedOutput()
 	if string(out) != "" && err != nil {
 		log.Println("[FATAL]", string(out))
 		showFatalError("An unexpected error occurred", "An unexpected error occurred while executing the \"lsof\" command.")
