82793911ae9b278e5809f0b7ccd156dbcee51449
typeling1578 <typeling1578@gmail.com>
2023-04-29 12:27:24 +0900

    [floorp_downloader] Use `got`

--- floorp_downloader/floorp_downloader.mjs
+++ floorp_downloader/floorp_downloader.mjs
@@ -1,21 +1,18 @@
-import fetch from "node-fetch";
+import got from "got";
 import fs from "fs";
-import {Headers} from 'node-fetch';
 
 (async() => {
-    let headers = new Headers();
+    let headers = {};
     if (process.env.GITHUB_TOKEN) {
-        console.log("Using `GITHUB_TOKEN`")
-        headers.append("Authorization", `Bearer ${process.env.GITHUB_TOKEN}`);
+        console.log("Using `GITHUB_TOKEN`");
+        headers["Authorization"] = `Bearer ${process.env.GITHUB_TOKEN}`;
     }
-    let result = await fetch("https://api.github.com/repos/Floorp-Projects/Floorp/releases/latest", {
-        method: "GET",
-        headers: headers,
-    });
-    if (result.status !== 200) {
-        throw `${result.status} ${result.statusText}`;
-    }
-    let result_obj = await result.json();
+    let result_obj = await got(
+        {
+            url: "https://api.github.com/repos/Floorp-Projects/Floorp/releases/latest",
+            headers: headers
+        }
+    ).json();
     let valid_asset;
     for (let asset of result_obj["assets"]) {
         if (process.argv[2] === "windows" && process.argv[3] === "x86_64") {
@@ -36,11 +33,11 @@ import {Headers} from 'node-fetch';
         }
     }
     if (!valid_asset) throw "Not found";
-    let result_binary = await fetch(valid_asset["browser_download_url"]);
-    if (result_binary.status !== 200) {
-        throw `${result_binary.status} ${result_binary.statusText}`;
-    }
-    let binary = Buffer.from(await result_binary.arrayBuffer());
+    let binary = await got(
+        {
+            url: valid_asset["browser_download_url"]
+        }
+    ).buffer();
     let output_filename;
     if (valid_asset["name"].endsWith(".tar.bz2")) {
         output_filename = "floorp-package.tar.bz2";
