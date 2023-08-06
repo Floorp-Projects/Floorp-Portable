80c1c0a0c33b5471b5181bb66dc11aecb1bff923
creeper-0910 <e12.riku.y@gmail.com>
2023-08-06 20:18:40 +0900

    fix error

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 echo Building redirector...
-go build -ldflags="-s -w"
+go build -ldflags="-s -w" -buildvcs=false
 
 echo Building patcher...
 cd patcher_src
-go build -ldflags="-s -w"
+go build -ldflags="-s -w" -buildvcs=false
 cp patcher ..
 cd ..
