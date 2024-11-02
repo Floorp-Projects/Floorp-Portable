78318fdbdff59c9020b479c0cd139907f1999ae0
typeling1578 <pub@typeling1578.dev>
2024-11-02 10:23:14 +0900

    Fix file in use detection

--- src/runtime/fileInUse_linux.go
+++ src/runtime/fileInUse_linux.go
@@ -23,6 +23,9 @@ func fileInUse(path string) bool {
 	}
 	out, err := cmd.CombinedOutput()
 	if err != nil {
+		if cmd.ProcessState.ExitCode() == 1 && len(out) == 0 {
+			return false
+		}
 		log.Printf("[FATAL] %s\n", out)
 		showFatalError("An unexpected error occurred", "An unexpected error occurred while executing the \"lsof\" command.")
 		panic(err)
