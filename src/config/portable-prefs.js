ee08e83b6bc564c9673c38cc453493394dc02b84
typeling1578 <pub@typeling1578.dev>
2024-11-02 19:42:10 +0900

    Update portable-prefs.js

--- src/config/portable-prefs.js
+++ src/config/portable-prefs.js
@@ -2,3 +2,6 @@
 
 pref("floorp.portable.enabled", true);
 pref("floorp.portable.update.enabled", true);
+
+// see: https://searchfox.org/mozilla-central/source/security/sandbox/common/SandboxUtils.sys.mjs#20
+pref("security.sandbox.warn_unprivileged_namespaces", false);
