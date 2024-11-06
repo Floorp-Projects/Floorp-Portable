93f2c98cdfe9d3b13a70d868d74e2011cc7131ad
typeling1578 <pub@typeling1578.dev>
2024-11-07 01:23:58 +0900

    add about page injection

---
+++ src/browser-modules/about-pages/AboutLicensePortable.sys.mjs
@@ -0,0 +1,34 @@
+/* -*- indent-tabs-mode: nil; js-indent-level: 2 -*-
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { nsIAboutModule } = Ci;
+
+export default function AboutLicensePortable() {}
+
+AboutLicensePortable.prototype = {
+  uri: Services.io.newURI("chrome://browser/content/license.html"),
+  classDescription: "about:license-portable",
+  contractID: "@mozilla.org/network/protocol/about;1?what=license-portable",
+  classID: Components.ID("{47e74de9-d25e-4eff-8256-0e9ec42c56ae}"),
+
+  QueryInterface: ChromeUtils.generateQI(["nsIAboutModule"]),
+
+  getURIFlags() {
+    return (
+      Ci.nsIAboutModule.URI_SAFE_FOR_UNTRUSTED_CONTENT |
+      Ci.nsIAboutModule.IS_SECURE_CHROME_UI
+    );
+  },
+
+  newChannel(uri, loadInfo) {
+    let channel = Services.io.newChannelFromURIWithLoadInfo(this.uri, loadInfo);
+    channel.owner = Services.scriptSecurityManager.getSystemPrincipal();
+    return channel;
+  },
+
+  getChromeURI(_uri) {
+    return this.uri;
+  },
+}
