768669adefebf381abcbd90d188a8fbfe61526a6
typeling1578 <pub@typeling1578.dev>
2024-11-08 02:06:23 +0900

    Update prefs

--- src/config/portable-prefs.js
+++ src/config/portable-prefs.js
@@ -1,7 +1,11 @@
 // Optimize for portable version
-
 pref("floorp.portable.enabled", true);
 pref("floorp.portable.update.enabled", true);
 
+pref("app.update.auto", false, locked);
+pref("app.update.background.enabled", false, locked);
+pref("browser.shell.checkDefaultBrowser", false, locked);
+pref("default-browser-agent.enabled", false, locked);
+
 // see: https://searchfox.org/mozilla-central/source/security/sandbox/common/SandboxUtils.sys.mjs#20
-pref("security.sandbox.warn_unprivileged_namespaces", false);
+pref("security.sandbox.warn_unprivileged_namespaces", false, locked);
