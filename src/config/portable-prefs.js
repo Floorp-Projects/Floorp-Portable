92486a30d7e42cde574f21e0b37691d95ffaf66b
typeling1578 <pub@typeling1578.dev>
2024-11-08 13:24:33 +0900

    set `toolkit.policies.perUserDir` to false

--- src/config/portable-prefs.js
+++ src/config/portable-prefs.js
@@ -8,3 +8,4 @@ pref("browser.shell.checkDefaultBrowser", false, locked);
 pref("default-browser-agent.enabled", false, locked);
 pref("browser.privacySegmentation.createdShortcut", true, locked); // see: https://searchfox.org/mozilla-esr128/source/browser/components/BrowserGlue.sys.mjs#2797
 pref("security.sandbox.warn_unprivileged_namespaces", false, locked); // see: https://searchfox.org/mozilla-central/source/security/sandbox/common/SandboxUtils.sys.mjs#20
+pref("toolkit.policies.perUserDir", false, locked);
