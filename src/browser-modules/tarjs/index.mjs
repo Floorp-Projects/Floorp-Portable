7cd973dd154055f5398c3e79c87afb3c9f987f31
typeling1578 <pub@typeling1578.dev>
2024-11-14 19:04:09 +0900

    Update tarjs library

--- src/browser-modules/tarjs/index.mjs
+++ src/browser-modules/tarjs/index.mjs
@@ -97,7 +97,7 @@ function readFileMode(buffer, offset) {
   // offset = 100, length = 8
   const view = new Uint8Array(buffer, offset + 100, 8);
   const modeStr = utf8Decode(view);
-  return parseInt(modeStr);
+  return parseInt(modeStr, 8);
 }
 function readFileUname(buffer, offset) {
   // offset = 265, length = 32
