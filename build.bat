470bf228455002e101df603f81315ca28b5c3021
typeling1578 <pub@typeling1578.dev>
2025-01-07 14:55:13 +0900

    fix[build]: use msys2 in local-building

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
 @echo off
-"C:\Program Files\Git\bin\bash.exe" -e -i build.sh
+if "%GITHUB_ACTIONS%"=="true" (
+  "C:\Program Files\Git\bin\bash.exe" -e -i build.sh
+) else (
+  "C:\msys64\usr\bin\env.exe" MSYS=winsymlinks:nativestrict CHERE_INVOKING=1 MSYSTEM=MINGW64 "C:\msys64\usr\bin\bash.exe" --login -e -i build.sh
+)
