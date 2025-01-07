7c77d2b87c9c482f5817a3eea68e777f40f17768
typeling1578 <pub@typeling1578.dev>
2025-01-07 13:35:54 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,6 @@
 @echo off
-"C:\Program Files\Git\bin\bash.exe" -e -i build.sh
+if %GITHUB_ACTIONS% == true (
+  "C:\Program Files\Git\bin\bash.exe" -e -i build.sh
+) else (
+  "C:\msys2\usr\bin\env.exe" MSYS=winsymlinks:nativestrict CHERE_INVOKING=1 MSYSTEM=MINGW64 "C:\msys2\usr\bin\bash.exe" -e -i build.sh
+)
