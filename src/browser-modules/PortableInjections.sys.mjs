202199d2784888dbe4b1cf85e44beb490874471c
typeling1578 <pub@typeling1578.dev>
2025-01-25 17:13:44 +0900

    enhance[bm]: add portable settings tab

--- src/browser-modules/PortableInjections.sys.mjs
+++ src/browser-modules/PortableInjections.sys.mjs
@@ -122,6 +122,23 @@ const documentObserver = {
         uriWithoutQueryRef.startsWith("chrome://noraneko-settings/")
       ) {
         const interval = window_.setInterval(async () => {
+          if (!document_.querySelector("#portable-tab-link")) {
+            const aboutTabLink = document_.querySelector('a[href="/about"]');
+
+            const cloned = aboutTabLink.cloneNode(true);
+            cloned.id = "portable-tab-link";
+            cloned.querySelector("div > p").innerText = "Portable Settings";
+            cloned.addEventListener("click", () => {
+              if (window_.location.pathname != "/portable") {
+                window_.history.pushState({}, "", "/portable");
+                // invoke React Router handler
+                window_.dispatchEvent(new PopStateEvent("popstate"));
+              }
+            });
+
+            aboutTabLink.insertAdjacentElement("beforebegin", cloned);
+          }
+
           if (window_.location.pathname  == "/about") {
             const aboutSectionTitle = document_.querySelector('img[alt="logo"][src="chrome://branding/content/about-logo@2x.png"] + p');
             const aboutSectionVersionInfo = document_.querySelector('div:has(>img[alt="logo"][src="chrome://branding/content/about-logo@2x.png"]) + p');
@@ -140,6 +157,20 @@ const documentObserver = {
 
               aboutSectionVersionInfo.insertAdjacentElement("beforebegin", portableVersionInfo);
             }
+          }
+
+          if (window_.location.pathname  == "/portable") {
+            if (!document_.querySelector("#portable-content")) {
+              const contentParent = document_.querySelector('div:has(>div>a[href="/about"]) + div > div');
+
+              const portableContent = document_.createElement("div");
+              portableContent.id = "portable-content";
+              portableContent.innerText = "test";
+
+              contentParent.appendChild(portableContent);
+            }
+          } else {
+            document_.querySelector("#portable-content")?.remove();
           }
         }, 1);
 
